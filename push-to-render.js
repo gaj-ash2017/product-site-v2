b670f15ac83bb482b84e58bac8bd02245e145250
Ashwin Gajan <gaj.ash2017@gmail.com>
2025-05-15 19:04:56 +0200

    a

--- push-to-render.js
+++ push-to-render.js
@@ -1,9 +1,9 @@
 const fs = require("fs");
-const fetch = require("node-fetch"); // install via npm install node-fetch@2
+const fetch = require("node-fetch"); // npm install node-fetch@2 if needed
 
 const messages = JSON.parse(fs.readFileSync("imported-messages.json", "utf-8"));
 
-fetch("https://product-site-v2.onrender.com/import-messages", {
+fetch("https://product-site-v2.onrender.com//import-messages", {
   method: "POST",
   headers: { "Content-Type": "application/json" },
   body: JSON.stringify(messages),
